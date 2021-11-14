28753c80d4d3576dc1f4eb570f73a92c80304efc
Ecotuc <ivan.cotuc@galileo.edu>
2021-11-13 21:50:54 -0600

    try2

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: '192.168.1.12',
   database: 'postgres',
   password: 'Abc12345',
   port: 5432,
