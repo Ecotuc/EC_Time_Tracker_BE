22af4d17f8f422cb67a7f056fefdd4e5b6eb2c2e
Ecotuc <ivan.cotuc@galileo.edu>
2021-11-14 22:06:49 -0600

    db change

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
 const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
-  host: '192.168.1.12',
-  database: 'postgres',
-  password: 'Abc12345',
+  host: 'localhost',
+  database: 'dbpf',
   port: 5432,
 })
 
